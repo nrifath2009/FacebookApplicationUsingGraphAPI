755c55c9712b7d3d505774bbb1b12f99f7228bc4
Nahid <nrifath2009@gmail.com>
2014-11-07 12:48:02 +0600

    New Project Uploaded

---
+++ FacebookApplicationUsingGraphAPI/FacebookApplicationUsingGraphAPI/Controllers/PostSearchController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Facebook;
+
+namespace FacebookApplicationUsingGraphAPI.Controllers
+{
+    public class PostSearchController : Controller
+    {
+        //
+        // GET: /PostSearch/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult PageInfo()
+        {
+            var accessToken = "CAACEdEose0cBAHlmZCe7S3v4bTvyk7ghAGmS0hwLSIlAsU2ZBbUgjPzpdhb9qNZBSLqHWFn3HZBlcgYVqeiOpkzfhB5pOj1V7Wdi2ZAjeylWTUyCSUctmqluvCgbaMeR8wxY1Bk9rExbFqXcBkBmQHdgZB8c8t0nOYwDsq62Xh6NwTVnEFmSVUMDOOWnxb8DY5Y3HZBSzoJGmxojwOJpJ2z4Trl7dFVSFcZD";
+            var client = new FacebookClient(accessToken);
+            dynamic me = client.Get("me");
+            return Json(me, JsonRequestBehavior.AllowGet);
+        }
+
+        
+	}
+}
---
+++ FacebookApplicationUsingGraphAPI/FacebookApplicationUsingGraphAPI/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacebookApplicationUsingGraphAPI.Models
+{
+    public class Post
+    {
+        public DateTime CreatedDateTime { get; set; }
+        public string ContainText { get; set; }
+
+    }
+}
